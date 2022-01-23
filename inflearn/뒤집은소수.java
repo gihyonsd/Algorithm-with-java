36dd35d06ffda6a76aa306beaa1e77ac2b22fccd
gihyonsd <48745104+gihyonsd@users.noreply.github.com>
2022-01-23 17:00:49 +0900

    Update 뒤집은소수.java

--- inflearn/뒤집은소수.java
+++ inflearn/뒤집은소수.java
@@ -12,7 +12,6 @@ public class 뒤집은소수 {
 		for(int i = 0; i < n; i++) {
 			a[i] = sc.next();
 		}
-		ArrayList<Integer> list = new ArrayList<>();
 		for(int i = 0; i < n; i++) {
 			int tmp = Integer.parseInt(new StringBuilder(a[i]).reverse().toString());
 			boolean prime = true;
