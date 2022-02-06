ab062d090220c3f1c119e93cd89f5a6656d3680d
gihyonsd <gihyonsd@gmail.com>
2022-02-06 18:07:20 +0900

    투포인터

---
+++ inflearn/두배열합치기fix.java
@@ -0,0 +1,42 @@
+package p31;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class 두배열합치기fix {
+	public static ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
+		ArrayList<Integer> answer = new ArrayList<>();
+		int p1 = 0, p2 = 0;
+		while(p1 < n && p2 < m) {
+			if(a[p1] < b[p2]) {
+				answer.add(a[p1++]);
+			}
+			else {
+				answer.add(b[p2++]);
+			}
+		}
+		while(p1 < n) {
+			answer.add(a[p1++]);
+		}
+		while(p2 < m) {
+			answer.add(b[p2++]);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for(int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+		int m = sc.nextInt();
+		int[] b = new int[m];
+		for(int i = 0; i < m; i++) {
+			b[i] = sc.nextInt();
+		}
+		for(int x : solution(n, m, a, b)) System.out.print(x+" ");
+	}
+
+}
