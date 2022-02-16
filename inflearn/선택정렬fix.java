06f20c6e4811a9f587af6c4421ebcb2db1dde193
gihyonsd <gihyonsd@gmail.com>
2022-02-16 22:51:39 +0900

    algo study

---
+++ inflearn/선택정렬fix.java
@@ -0,0 +1,30 @@
+package p61;
+
+import java.util.Scanner;
+
+public class 선택정렬fix {
+	public static int[] solution(int n, int[] arr) {
+		for(int i = 0; i < n - 1; i++) {
+			int idx = i;
+			for(int j = i + 1; j < n; j++) {
+				if(arr[j] < arr[idx]) idx = j;
+			}
+			int tmp = arr[i]; //swap
+ 			arr[i] = arr[idx];
+ 			arr[idx] = tmp;
+		}
+		
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int x : solution(n, arr)) System.out.print(x + " ");
+	}
+
+}
