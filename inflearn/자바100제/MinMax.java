6fbe662c92726e1a2e16d22ba3ed89a15395d275
gihyonsd <gihyonsd@gmail.com>
2022-03-13 22:26:44 +0900

    algo study

---
+++ inflearn/자바100제/MinMax.java
@@ -0,0 +1,30 @@
+package 자바100제;
+
+import java.util.Scanner;
+
+public class MinMax {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int max = arr[0];
+		int min = arr[0];
+		for(int i = 0; i < n; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+			if(min > arr[i]) {
+				min = arr[i];
+			}
+		}
+		System.out.println("배열내 최댓값 : "+max);
+		System.out.println("배열내 최소값 : "+min);
+		
+	}
+
+}
