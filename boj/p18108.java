8de85617007972778899e1aefdf0dc7dbca6ccf7
gihyonsd <gihyonsd@gmail.com>
2022-07-08 22:27:21 +0900

    algo study

---
+++ boj/p18108.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class p18108 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = n - 543;
+		System.out.println(m);
+	}
+
+}
